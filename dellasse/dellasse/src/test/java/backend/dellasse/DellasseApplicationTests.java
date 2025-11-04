d2432fbe069c066270a46fb7f3b80feddef84154
LuizChristani <166182040+LuizChristani@users.noreply.github.com>
2025-11-04 19:30:03 -0300

    feat: add backend dependencies

---
+++ dellasse/dellasse/src/test/java/backend/dellasse/DellasseApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.dellasse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DellasseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
