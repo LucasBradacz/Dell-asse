5afdd56e3258d0865e52432cd3b2d88190937e04
Soldado de Guerra <166182040+LuizChristani@users.noreply.github.com>
2025-11-06 19:31:54 -0300

    feat: add model user for database

---
+++ backend/src/main/java/com/dellasse/backend/models/Users.java
@@ -0,0 +1,49 @@
+package com.dellasse.backend.models;
+
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String uuid;
+
+    private String name;
+    private String email;
+    private String username;
+    private String password;
+
+    @ManyToMany
+    @JoinTable(
+        name = "user_roles",
+        joinColumns = @JoinColumn(name = "user_id"),
+        inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles;
+    
+    @ManyToOne
+    private Enterprise enterprise;
+    
+}
