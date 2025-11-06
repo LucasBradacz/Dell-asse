d7391ea0a06ed37cb0cd23c6e70770cac93c1fdf
Soldado de Guerra <166182040+LuizChristani@users.noreply.github.com>
2025-11-06 19:32:06 -0300

    feat: add model roles for database

---
+++ backend/src/main/java/com/dellasse/backend/models/Role.java
@@ -0,0 +1,42 @@
+package com.dellasse.backend.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Long roleId;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Getter
+    public enum Values {
+        ADMIN(1L),
+        FUNCIONARIO(2L),
+        BASIC(3L);
+
+        private final long roleId;
+
+        Values(long roleId) {
+            this.roleId = roleId;
+        }
+
+    }
+    
+}
