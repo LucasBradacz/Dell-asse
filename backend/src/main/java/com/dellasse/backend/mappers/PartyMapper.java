efc24e769c0fa0ce70a5d611c77740505ad8f2bd
Soldado de Guerra <166182040+LuizChristani@users.noreply.github.com>
2025-11-15 23:02:33 -0300

    fix: mapper error in gitHub

--- backend/src/main/java/com/dellasse/backend/mappers/PartyMapper.java
+++ backend/src/main/java/com/dellasse/backend/mappers/PartyMapper.java
@@ -1,36 +1,25 @@
 package com.dellasse.backend.mappers;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.NullValuePropertyMappingStrategy;
-
 import com.dellasse.backend.contracts.party.PartyCreateRequest;
+import com.dellasse.backend.contracts.party.PartyUpdateRequest;
+
 import com.dellasse.backend.models.Party;
 
 @Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface PartyMapper {
-<<<<<<< Updated upstream
-    public static Party toEntity(PartyCreateRequest create) {
-        return new Party(
-            create.title(),
-            create.description(),
-            create.productList().stream().map(productId -> new Product(productId)).toList(),
-            create.generateBudget(),
-            create.observations(),
-            create.imageURL()
-        );
-    }
-=======
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "lastAtualization", ignore = true)
     @Mapping(target = "status", ignore = true)
-    Party toEntity(CreateRequest createRequest);
+    Party toEntity(PartyCreateRequest createRequest);
 
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "lastAtualization", ignore = true)
     @Mapping(target = "status", ignore = true)
     @Mapping(target = "productList", ignore = true)
     @Mapping(target = "user", ignore = true)
-    Party toEntity(UpdateRequest updateRequest);
->>>>>>> Stashed changes
+    Party toEntity(PartyUpdateRequest updateRequest);
 }
 
